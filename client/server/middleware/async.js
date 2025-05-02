b44f1d985b317536b781a1c236edbc3ebeb0a749
Slowpokess <68640835+Slowpokess@users.noreply.github.com>
2025-05-02 09:43:41 +0300

    Удаление ненужных папок client и ggg

--- client/server/middleware/async.js
+++
@@ -1,6 +0,0 @@
-// Middleware для асинхронных обработчиков
-// Обертывает асинхронные функции контроллеров для единообразной обработки ошибок
-const asyncHandler = fn => (req, res, next) =>
-  Promise.resolve(fn(req, res, next)).catch(next);
-
-module.exports = asyncHandler;
